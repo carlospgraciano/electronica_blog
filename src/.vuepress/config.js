c90acdb0f32befc265bc22dd8a6cd5a4d74829f2
Carlos Polanco <carlosfollows@gmail.com>
2020-10-22 21:22:23 -0400

    Upload and config the favicons for the blog

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Blog de Electrónica',
+  title: 'Blog de electrónica',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -15,10 +15,18 @@ module.exports = {
    *
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
+
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicons/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicons/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/images/favicons/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/images/favicons/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
   ],
 
   /**
